2beda697fd2d94327742f356dc30f1bf05e2fbb6
desto12 <urban.bartosz21@gmail.com>
2019-04-28 09:50:28 +0200

    Enemy detection, added animations

--- RPG Game Project/Assets/Scripts/PlayerControl.cs
+++ RPG Game Project/Assets/Scripts/PlayerControl.cs
@@ -5,28 +5,88 @@ using UnityEngine.AI;
 
 public class PlayerControl : MonoBehaviour
 {
+    public float attackDistance;
+    public float attackRate;
+    private float nextAttack;
+
     private NavMeshAgent navMeshAgent;
     private Animator anim;
+
+    private Transform targetedEnemy;
+    private bool walking;
+    private bool enemyClicked;
 
     // Start is called before the first frame update
     void Start()
     {
-        anim = GetComponent<Animator>();
+        anim = GetComponentInChildren<Animator>();
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         if (Input.GetButtonDown("Fire2"))
         {
-            if (Physics.Raycast(ray, out hit))
+            if (Physics.Raycast(ray, out hit, 1000))
             {
-                navMeshAgent.SetDestination(hit.point);
+                if (hit.collider.tag == "Enemy")
+                {
+                    targetedEnemy = hit.transform;
+                    enemyClicked = true;
+                }
+                else
+                {
+                    walking = true;
+                    enemyClicked = false;
+                    navMeshAgent.isStopped = false;
+                    navMeshAgent.destination = hit.point;
+                }
             }
         }
 
+        if (enemyClicked)
+        {
+            MoveAndAttack();
+        }
+
+        if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+        {
+            walking = false;
+        }
+        else
+            walking = true;
+        anim.SetBool("Run", walking);
+    }
+
+    void MoveAndAttack()
+    {
+        if(targetedEnemy == null)
+        {
+            return;
+        }
+
+        navMeshAgent.destination = targetedEnemy.position;
+        if(navMeshAgent.remainingDistance > attackDistance)
+        {
+            navMeshAgent.isStopped = false;
+            Debug.Log("hited");
+            walking = true;
+        }
+        else
+        {
+            transform.LookAt(targetedEnemy);
+            Vector3 attackdir = targetedEnemy.transform.position - transform.position;
+
+            if (Time.time > nextAttack) ;
+            {
+                nextAttack = Time.time + attackRate;
+            }
+            navMeshAgent.isStopped = true;
+            walking = false;
+        }
     }
 }
